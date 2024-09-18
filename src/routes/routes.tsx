89c4af7b17af88c32d6edb345cab01c0655c7113
Kacueto <75464308+Kacueto@users.noreply.github.com>
2024-09-18 14:40:12 +0000

    Change the title of the page and fix the problem with the path base

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages/Home";
 
+const Base = '/Dragons-2024-2/'
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: Base,
     element: <Home />,
   }
 ]);
