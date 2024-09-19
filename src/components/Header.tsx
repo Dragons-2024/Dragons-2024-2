760fcd39d387e9a6b2f1af9066a3025c7ad5b3dc
Kacueto <75464308+Kacueto@users.noreply.github.com>
2024-09-19 01:57:26 +0000

    Take the html structure from the header past and put it in the Header.tsx file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,21 @@
 export function Header() {
     return (
         <header>
-            <h1>Dragons</h1>
+            <section >
+                <div >
+                    <a href="#"><img src="" alt="logo"/></a>
+                </div>
+                <div >
+                    <input type="text" placeholder="Buscar"/>
+                        <button >Buscar</button>
+                </div>
+                <nav >
+                    <ul>
+                        <li><a href="#"><i className=""></i>Mi Cuenta</a></li>
+                        <li><a href="#"><i className=""></i>Carrito</a></li>
+                    </ul>
+                </nav>
+            </section>
         </header>
     );
 }
