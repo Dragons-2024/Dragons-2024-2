265bee3f8e00ecf481cde5518710befe81ffdc29
Julian Almario <juliandavidalmario@gmail.com>
2024-09-18 20:37:12 -0500

    Component finish

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { Main } from "../layout/Main";
+import { promo_images } from "../utils/promotional_img_data";
 
 export function Home() {
   return (
@@ -6,6 +7,11 @@ export function Home() {
       <section>
         <h2>Home</h2>
         <p>Welcome to the Dragons website.</p>
+        <section className="w-full h-auto overflow-hidden">
+          {promo_images.map((image)=>(
+            <img key={image.id} src={image.img} alt={image.alt} />
+          ))}
+        </section>
       </section>
     </Main>
   );
