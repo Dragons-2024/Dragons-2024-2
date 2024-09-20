a816859265bbeca03237ea9cc797121af8814a5f
estebanramirez13 <ecpereira@uninorte.edu.co>
2024-09-20 18:11:34 -0500

    guardado de prevencion para que no se pierda lo que llevo

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -11,25 +11,27 @@ interface ProductItemProps {
 
 const ProductItem: React.FC<ProductItemProps> = ({ img, name, rating, features, normalPrice, discountedPrice }) => {
   return (
-    <div className="border border-gray-200 rounded-md p-4 mb-4 transition-shadow duration-300 flex flex-col items-center text-center">
+    <div className="border border-gray-300 rounded-lg p-6 mb-6 transition-shadow duration-300 hover:shadow-lg bg-white w-full max-w-[900px] flex flex-col items-center">
       <img
         src={img}
         alt={name}
-        className="max-h-full max-w-[200px] object-contain mb-4"
+        className="h-48 w-48 object-contain mb-4"
       />
-      <h2 className="text-xl font-bold mb-2">{name}</h2>
-      <div className="flex items-center justify-center mb-2">
-        <i className="fas fa-star text-yellow-500"></i>
-        <span className="ml-1 text-lg">{rating}</span>
+      <div className="flex flex-col justify-between items-center text-center w-full">
+        <h2 className="text-2xl font-semibold mb-2">{name}</h2>
+        <div className="flex items-center text-yellow-500 mb-2">
+          <i className="fas fa-star"></i>
+          <span className="ml-2 text-lg">{rating}</span>
+        </div>
+        <p className="text-gray-600 text-sm mb-4">{features}</p>
+        <div className="flex items-center mb-4">
+          <span className="text-gray-500 line-through text-sm mr-4">{normalPrice}</span>
+          <span className="text-blue-600 text-2xl font-bold">{discountedPrice}</span>
+        </div>
+        <button className="bg-yellow-500 text-white py-2 px-6 rounded-full hover:bg-yellow-600 focus:outline-none">
+          Añadir al Carrito
+        </button>
       </div>
-      <p className="text-gray-700 mb-2">{features}</p>
-      <div className="flex items-center justify-center mb-4">
-        <span className="text-gray-500 line-through mr-2">{normalPrice}</span>
-        <span className="text-blue-500 text-xl font-bold">{discountedPrice}</span>
-      </div>
-      <button className="bg-yellow-500 text-white py-2 px-4 rounded-full hover:bg-yellow-600">
-        Añadir al Carrito
-      </button>
     </div>
   );
 };
--- src/components/ProductListPLP.tsx
+++ src/components/ProductListPLP.tsx
@@ -25,7 +25,7 @@ const ProductListPLP: React.FC = () => {
           <option>Precio mayor a menor</option>
         </select>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-8">
         {products.map((product, index) => (
           <ProductItem
             key={index}
