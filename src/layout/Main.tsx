3677bc7a28441f652ed00f320712b2027eb221bb
estebanramirez13 <ecpereira@uninorte.edu.co>
2024-09-22 14:07:41 -0500

    borrador

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,13 +1,18 @@
+import React from 'react';
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
+import FilterBox from "../components/FilterBox";
 
 export const Main = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <div className="flex flex-col min-h-screen">
         <Header />
-        <main className="flex-grow text-2xl bg-[#F5F5F5] text-[#555555]">
-          {children}
+        <main className="flex-grow text-2xl bg-[#F5F5F5] text-[#555555] flex">
+          <FilterBox />
+          <div className="flex-grow">
+            {children}
+          </div>
         </main>
         <Footer />
       </div>
