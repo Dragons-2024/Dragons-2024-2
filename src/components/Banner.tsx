1f9c28aac2b758d1d4871e9911b50ec4aec81c3e
estebanramirez13 <ecpereira@uninorte.edu.co>
2024-09-22 16:10:13 -0500

    ahi voy

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import promo4 from '../components/promo4.png';
 const Banner: React.FC = () => {
   return (
     <div className="banner text-center my-8">
-      <img src="img/promo4.png" alt="Banner Publicitario" className="w-full h-auto rounded-lg" />
+      <img src={promo4} alt="Banner Publicitario" className="w-full h-auto rounded-lg" />
     </div>
   );
 };
