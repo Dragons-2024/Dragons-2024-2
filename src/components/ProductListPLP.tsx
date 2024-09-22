19d2f57b660edc186e57c74b946d1da14f6dbe9a
estebanramirez13 <ecpereira@uninorte.edu.co>
2024-09-22 13:09:34 -0500

    Update ProductListPLP.tsx

--- src/components/ProductListPLP.tsx
+++ src/components/ProductListPLP.tsx
@@ -131,19 +131,19 @@ const ProductListPLP = () => {
         {/* Aquí puedes agregar contenido adicional para el aside */}
       </aside>
       <section className="w-full md:w-3/4 xl:w-4/5 p- md:ml">
-        <div className="list-header flex justify-between items-center mb-4">
-          <span>Mostrando {sortedProducts.length} resultados de 35</span>
-          <div className="flex space-x-4">
-          <select className="border border-gray-300 rounded p-2" onChange={handleSortChange}>
-  <option value="default">Ordenar por Defecto</option>
-  <option value="rating">Ordenar por Calificación</option>
-  <option value="priceAsc">Ordenar por Precio: Menor a Mayor</option>
-  <option value="priceDesc">Ordenar por Precio: Mayor a Menor</option>
-  <option value="discount">Ordenar por Mayor Descuento</option>
-</select>
+      <div className="list-header flex flex-col md:flex-row justify-between items-start md:items-center mb-4">
+  <span>Mostrando {sortedProducts.length} resultados de {products.length}</span>
+  <div className="flex space-x-4 mt-2 md:mt-0">
+    <select className="border border-gray-300 rounded p-2" onChange={handleSortChange}>
+      <option value="default">Ordenar por Defecto</option>
+      <option value="rating">Ordenar por Calificación</option>
+      <option value="priceAsc">Ordenar por Precio: Menor a Mayor</option>
+      <option value="priceDesc">Ordenar por Precio: Mayor a Menor</option>
+      <option value="discount">Ordenar por Mayor Descuento</option>
+    </select>
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-4">
+        <div className="grid grid-cols-1 gap-4">
           {sortedProducts.map((product, index) => (
             <ProductItem
               key={index}
