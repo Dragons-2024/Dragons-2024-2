9e8ee22667b6377f26ad69faa312a334c4415614
estebanramirez13 <ecpereira@uninorte.edu.co>
2024-09-22 18:34:14 -0500

    Update ProductListPLP.tsx

--- src/components/ProductListPLP.tsx
+++ src/components/ProductListPLP.tsx
@@ -147,7 +147,7 @@ export const ProductListPLP = () => {
               </select>
             </div>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-4">
             {sortedProducts.map((product, index) => (
               <ProductItem
                 key={index}
