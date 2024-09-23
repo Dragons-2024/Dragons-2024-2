45eb39bf03c345b22e63498dff5e638299a98738
Kacueto <75464308+Kacueto@users.noreply.github.com>
2024-09-23 03:11:10 +0000

    finish HU2006-Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,27 @@
-
-
+import { FaSearch } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import Logo from '../assets/magisterlogoblanco.png'
 export function Header() {
     return (
         <header>
-            <section className='flex h-24 items-center bg-[#1E2759]' >
-                <div className='flex-1 h-4/5 items-center py-2 px-8'   >
+            <section className='flex gap-2 md:h-24 items-center bg-[#1E2759] gap-12' >
+                <div className='h-full py-0 px-0 ml-4 md:h-4/5 items-center py-2 px-8 mr-8'   >
                     <Link to="#">
                         <img className="h-full w-auto" src={Logo} alt="Logo" />
                     </Link>
                 </div>
-                <div className=' flex flex-1 hmax-w-screen-md '>
+                <div className='hidden md:flex flex-1 items-center max-w-screen-sm gap-2'>
                     <input className='rounded-2xl h-8 w-full px-4' type="text" placeholder="Buscar" />
-                    <button className='bg-[#4A5FD9] rounded-2xl px-4 py-2 text-white' >Buscar</button>
+                    <button className='bg-[#4A5FD9] rounded-2xl px-6 py-3 text-white hover:bg-[#F2F2F2] hover:text-black' ><FaSearch /></button>
                 </div>
-                <nav className='flex-1'>
-                    <ul className='flex'>
-                        <li className=""><a href="#"><i ></i>Mi Cuenta</a></li>
-                        <li  className=""><a href="#"><i></i>Carrito</a></li>
+                <nav className=''>
+                    <ul className='flex items-center gap-2 md:gap-8 ml-8 ' >
+                        <li className="text-white hover:text-black "><a className="flex items-center gap-2" href="#"><i ></i>
+                            <FaUser />
+                            Mi Cuenta</a></li>
+                        <li className="text-white hover:text-black"><a className="flex items-center gap-2" href="#"><i></i><FaShoppingCart />Carrito</a></li>
                     </ul>
                 </nav>
             </section>
