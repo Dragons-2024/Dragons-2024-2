9c39faa6d6fbf38aa99ba09fe332dafc3779d03e
Lena Castillo <lenac@uninorte.edu.co>
2024-09-27 21:00:05 -0500

    HU2018-Fix-Render

--- src/components/ListProducts.tsx
+++ src/components/ListProducts.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+ {/* import React from 'react';
 import FeaturedProduct from './FeaturedProducts';
 import { FeaturedProductsListProps } from '../core/interfaces/FeaturedProductsInter';
 
@@ -13,5 +13,37 @@ const FeaturedProductsList: React.FC<FeaturedProductsListProps> = ({ products })
   );
 };
 
-export default FeaturedProductsList;
+export default FeaturedProductsList;*/}
 
+import React, { useEffect, useState } from 'react';
+import { axiosApi } from '../api/axios'; 
+import FeaturedProduct from './FeaturedProducts';
+import { FeaturedProductsListProps } from '../core/interfaces/FeaturedProductsInter';
+
+
+const FeaturedProductsList: React.FC = () => {
+  const [products, setProducts] = useState<FeaturedProductsListProps['products']>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axiosApi.get('http://localhost:3000/FeaturedProducts');
+        setProducts(response.data); 
+      } catch (error) {
+        console.error('Error fetching the products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-7 p-4">
+      {products.map(product => (
+        <FeaturedProduct key={product.id} {...product} />
+      ))}
+    </div>
+  );
+};
+
+export default FeaturedProductsList;
