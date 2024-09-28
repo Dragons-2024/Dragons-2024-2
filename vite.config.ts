f6472fbf0608f467aa801de84ec7c5b4f4decd47
Kacueto <75464308+Kacueto@users.noreply.github.com>
2024-09-28 02:38:46 +0000

    change in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  preview: {
-    port: Number(process.env.PORT!) || 4173, 
-    host: true 
-  }
+  
 });
