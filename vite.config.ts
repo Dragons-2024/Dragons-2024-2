3355c486e790f49f4a4e6c908177694d449a82c0
Kacueto <75464308+Kacueto@users.noreply.github.com>
2024-09-28 03:02:42 +0000

    revert in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  
+  base: '/Dragons-2024-2/',
+  preview: {
+    port: Number(process.env.PORT!) || 4173, 
+    host: true 
+  }
 });
