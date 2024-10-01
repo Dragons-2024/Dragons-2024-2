f72ed0abf8195a84bda195da384825bc87250d3e
Lena Castillo <lenac@uninorte.edu.co>
2024-09-30 19:55:40 -0500

    Fix-Color-Loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import { BallTriangle } from 'react-loader-spinner';
 export const Loading: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <BallTriangle height="100" width="100" color="Yellow" ariaLabel="loading" />
+      <BallTriangle height="100" width="100" color="#FFA600" ariaLabel="loading" />
     </div>
   );
 };
