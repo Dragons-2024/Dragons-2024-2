80d464794c8d35a60cca4593afff4e00ea112d57
Julian Almario <juliandavidalmario@gmail.com>
2024-10-04 00:30:25 -0500

    Products especifications finish

--- src/components/ProductEspecification.tsx
+++ src/components/ProductEspecification.tsx
@@ -14,13 +14,13 @@ export function ProductEspecification({details}:DetailsProductsProps){
    return (
    <section className=" my-10 max-[768px]:mx-auto  max-[768px]:my-0">
       <h2 className="text-blue-800 text-2xl max-[768px]:text-center font-semibold">Especificaciones tecnicas</h2>
-    <table className="text-lg text-black mt-5 w-full max-[768px]:text-center border-2 max-[768px]:text-base ">
+    <table className="text-lg text-black mt-5 w-full max-[768px]:text-center max-[768px]:text-xs box-border ">
 
       <tbody>
       {Object.entries(details).map(([key,value])=>(
          <tr key={key}>
-          <td className="text-left text-blue-800 w-2/5 font-semibold pr-5 max-[768px]:p-5">{key}</td>
-          <td className=" text-left pr-5 max-[768px]:p-0">{value}</td>
+          <td className="text-left text-blue-800 font-semibold  py-2 ">{key}</td>
+          <td className=" text-left py-2 max-[426px]:text-right">{value}</td>
          </tr>
        ))}
       </tbody>
