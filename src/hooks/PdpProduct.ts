a19fb612a54f03d3d63490e7078b967594614b36
kacueto <kacueto@uninorte.edu.co>
2024-10-04 23:32:51 -0500

    filter component without stack query

--- src/hooks/PdpProduct.ts
+++ src/hooks/PdpProduct.ts
@@ -1,21 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 import { getPdpProductFeatured, getPdpProductPlp } from '../services/PdpService';
 
-type usePlpProductProps={
- productId:number;
- category:String;
+type usePlpProductProps = {
+  productId: number;
+  category: String;
 }
-export const usePdpProduct = ({productId,category}:usePlpProductProps) => {
-  if (category===""){
+export const usePdpProduct = ({ productId, category }: usePlpProductProps) => {
+  if (category === "") {
     return useQuery({
       queryKey: ['PdpProduct', productId], // Agregar el productId a la clave de consulta
-      queryFn: ()=>getPdpProductFeatured(productId),
+      queryFn: () => getPdpProductFeatured(productId),
     });
-  }else{
-      return useQuery({
-        queryKey: ['PdpProduct', productId,category], // Agregar el productId a la clave de consulta
-        queryFn: ()=>getPdpProductPlp({id:productId,category}),
-      });
-    }
-  
+  } else {
+    return useQuery({
+      queryKey: ['PdpProduct', productId, category], // Agregar el productId a la clave de consulta
+      queryFn: () => getPdpProductPlp({ id: productId, category }),
+    });
+  }
+
 };
